20acc4aebb1fb77fe45185eef0a724b6c1fc3680
Mona Lisa <amathenu06@gmail.com>
2026-02-22 21:21:19 +0530

    Added file promises feature

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 console.log("Lab 03 - NodeJS Setup Successful!");
+const condition = true;
 
-const myModule = require('./my-module.js');
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
 
-console.log(myModule.myFunction());
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 
 
 
