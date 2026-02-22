baa2ae618e06932d72e78065ccff5e8af9e7aad0
Mona Lisa <amathenu06@gmail.com>
2026-02-22 21:18:58 +0530

    Added file module feature

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 console.log("Lab 03 - NodeJS Setup Successful!");
-const https = require('https');
 
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-  let data = '';
+const myModule = require('./my-module.js');
 
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-  });
-
-}).on('error', (err) => {
-  console.log("Error: " + err.message);
-});
+console.log(myModule.myFunction());
 
 
 
